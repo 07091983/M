4cee7c2e207f83224601615e8fc8350113a5dd4e
07091983 <126685467+07091983@users.noreply.github.com>
2024-03-10 11:54:34 +0700

    Update MainComponent.java

--- src/main/java/org/example/MainComponent.java
+++ src/main/java/org/example/MainComponent.java
@@ -1,2 +1,19 @@
-package org.example;public class MainComponent {
+package org.example;
+public abstract class MainComponent {
+
+    public void add(MainComponent mainComponent){
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Integer getPages(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
 }

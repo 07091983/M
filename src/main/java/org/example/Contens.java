ab257cc4274ab04c7be0b9fdcb0e0447f1e93891
07091983 <126685467+07091983@users.noreply.github.com>
2024-03-10 11:53:12 +0700

    Update Contens.java

--- src/main/java/org/example/Contens.java
+++ src/main/java/org/example/Contens.java
@@ -1,2 +1,24 @@
-package org.example;public class Contens {
-}
+package org.example;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Contens extends MainComponent{
+    ArrayList<MainComponent> mainComponents = new ArrayList<MainComponent>();
+    String name;
+    Integer pages;
+
+    public Contens(String name,Integer pages){
+
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public void add(MainComponent mainComponent){
+        mainComponents.add(mainComponent);
+    }
+    public String getName(){
+        return name;
+    }
+    public Integer getPages(){
+        return pages;
+    }

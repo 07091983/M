ded49b82e7534725f7bca54fa61ad1d0d988ae03
07091983 <126685467+07091983@users.noreply.github.com>
2024-03-10 11:56:39 +0700

    Update Book.java

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,2 +1,11 @@
-package org.example;public class Book {
+package org.example;
+public class Book {
+    MainComponent contents;
+
+    public Book(MainComponent contents) {
+        this.contents = contents;
+    }
+    public void printContents() {
+        contents.print();
+    }
 }

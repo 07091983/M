b3d3063e48e7facabbb4bc53bb25da291e425853
07091983 <126685467+07091983@users.noreply.github.com>
2024-03-10 11:55:47 +0700

    Update Paragraphs.java

--- src/main/java/org/example/Paragraphs.java
+++ src/main/java/org/example/Paragraphs.java
@@ -1,2 +1,33 @@
-package org.example;public class Paragraphs {
+package org.example;
+import java.util.ArrayList;
+
+public class Paragraphs extends MainComponent{
+    ArrayList<MainComponent> mainComponents = new ArrayList<MainComponent>();
+    String name;
+    Integer pages;
+
+    public Paragraphs(String name,Integer pages){
+
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public void add(MainComponent mainComponent){
+        mainComponents.add(mainComponent);
+    }
+    public String getName(){
+        return name;
+    }
+    public Integer getPages(){
+        return pages;
+    }
+    public void print() {
+        System.out.print("  " + getName());
+        for (int i = 0; i < 68-getName().length(); i++) {
+            System.out.print(".");
+        }
+        System.out.println(getPages());
+    }
+
+
 }
